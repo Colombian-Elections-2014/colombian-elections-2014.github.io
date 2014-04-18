855d9506556132491052c40566b4c7c0e3fd28e8
Michael Bauer <mihi@lo-res.org>
2014-04-18 12:22:16 -0400

    basic elections infrastructure -- data loading and filtering works...

--- js/elections.js
+++ js/elections.js
@@ -3,5 +3,22 @@ function draw() {
     var sheet="https://docs.google.com/spreadsheet/pub?hl=en&hl=en&key=0Aq9agjil66PydHNLUkFaYXBUdWJrT1ZJRjBRbjJOMUE&single=true&gid=0&output=csv"
     d3.csv(sheet,function(data)
         {
-        console.log(data) 
+        
+        // visualization for ingresos
+        var ingresos=_.filter(data, function(x) {
+            return x.Tipo == "ingreso"; 
+            });
+        console.log(ingresos);
+        
+
+
+        // visualization for gastos
+        var gastos=_.filter(data, function(x) {
+            return x.Tipo == "gastos";
+            });
+        
+        console.log(gastos);
+        });
         }
+
+document.body.onload=draw();        
